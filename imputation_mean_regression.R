24be0c14187384737269578a2c36304e7eb793b6
Sabrina Lopes Franca <sabrina.loopes23@gmail.com>
2023-10-16 14:03:39 -0300

    perform imputation with simulation data

--- imputation_mean_regression.R
+++ imputation_mean_regression.R
@@ -1,13 +1,12 @@
 
-# Funcao: realiza a simulacao dos dados, 
-# reduz os dados (transforma em dados faltantes) e faz a imputacao por
-# media e por regressao linear
+# Function: does the data simulation, reduces the data (introduzing na's)
+# and perform mean imputation and linear regression imputation
 
 imput_func  <- function(n,p){
-# n = tamanho da amostra
-# p = proporcao de dados faltantes
+# n = sample size
+# p = missing data proportion
   
-m <- 1000  ## n de réplicas de monte carlo
+m <- 1000  ## monte carlo replicas
 EQM <- EQM.media <- EQM.RL <- numeric(m)
 EAM <- EAM.media <- EAM.RL <- numeric(m)
 
@@ -15,49 +14,50 @@ set.seed(22222)
 
 for (i in 1:m){
   
-  ### geração dos dados
-  x2 <- rbinom(n, 1, 0.4) ## covariavel binaria com prob. de sucesso p = 0.4
-  x3 <- rexp(n,.2) ## covariavel gerada da dist. exponencial com lambda = 0.2
-  x1 <- 2*x2 - .1*x3 + rnorm(n,0,.5)  ## covariável que depende de x2 e x3 acrescido
-  # de um ruído gerado pela  dist .normal
+  ### generating data
+  x2 <- rbinom(n, 1, 0.4) ## binary covariate with probability of success p = 0.4
+  x3 <- rexp(n,.2) ## covariate generated from the exponential distri. with lambda = 0.2
+  x1 <- 2*x2 - .1*x3 + rnorm(n,0,.5)  ##  covariate that depends on x2 and x3 
+  # added an error generated from the normal distribution
   
-  y <- 3*x1 - 0.5*x2 + x3 + rnorm(n,0,5) ## variavel resposta do modelo
+  y <- 3*x1 - 0.5*x2 + x3 + rnorm(n,0,5) ## response varible of the model
   
   dados <- as.data.frame(cbind(y,x1,x2,x3)) 
   
-  est <-lm(y~x1+x2+x3) ## modelo de regressao
-  prev <- predict(est) ## realizando a predicao com o modelo
+  est <-lm(y~x1+x2+x3) ## regression model
+  prev <- predict(est) ## doing the prediction
   
-  EQM[i]<-mean((y-prev)^2) ## erro quadratico medio
-  EAM[i]<-mean(abs(y-prev)) ## erro absoluto medio
+  EQM[i]<-mean((y-prev)^2) ## mean square error
+  EAM[i]<-mean(abs(y-prev)) ## mean absolute error
   
   
   missings <- sample(1:n, p*n, replace = FALSE)
-  d.red <- dados[-missings,] ## dados reduzidos
+  d.red <- dados[-missings,] ## data reduced
   
   
-  #### imputação pela media
+  #### mean imputation
   DImedia <- dados
-  DImedia$x1[missings] <- mean(d.red$x1) ## imputando pela media de x1
+  DImedia$x1[missings] <- mean(d.red$x1) ## imputing with the x1 mean
   
-  est.media<-lm(DImedia$y~DImedia$x1+DImedia$x2+DImedia$x3) ## gerando modelo 
-  # com os dados imputados
+  est.media<-lm(DImedia$y~DImedia$x1+DImedia$x2+DImedia$x3) ## generating model
+  #with the data imputaded
   prev.media<-predict(est.media)
   
   EQM.media[i]<-mean((DImedia$y-prev.media)^2)
   EAM.media[i]<-mean(abs(DImedia$y-prev.media))
   
   
-  ### imputação por regressao linear
+  ### linear regression imputation
   est.glm.RL <- lm(d.red$x1 ~ d.red$x2 + d.red$x3) 
   
-  d.missing <- dados[missings,]   ## dados missings para serem imputados
-  x1.imput <- est.glm.RL$coef[1] + est.glm.RL$coef[2]*d.missing$x2 + est.glm.RL$coef[3]*d.missing$x3
+  d.missing <- dados[missings,]   ##  missing data to be imputed
+  x1.imput <- est.glm.RL$coef[1] + est.glm.RL$coef[2]*d.missing$x2 +
+    est.glm.RL$coef[3]*d.missing$x3
   
   DI.RL <- dados
   DI.RL$x1[missings] <- x1.imput
   
-  ## gerando o modelo com os novos dados imputados
+  ## generating the model with the new imputed data
   est.RL <- lm(DI.RL$y ~ DI.RL$x1 + DI.RL$x2 + DI.RL$x3)
   prev.RL <- predict(est.RL)
   
@@ -79,8 +79,8 @@ return(data.frame("EQM" = mean(EQM),
 
 }
 
-### Funcao auxiliar que retorna a imputacao para todas
-# as proporcoes de dados desejados
+### Auxiliar function that returns the imputation for every 
+# desired proportion of missing data
 
 func2 <- function(n){
   t_l <- imput_func(n, 0.2)
@@ -94,7 +94,7 @@ func2 <- function(n){
 }
 
 
-### Dataframe final com os tamanhos de amostra desejados
+### Final dataframe with every desired samples sizes
 data_imput <- rbind(func2(20),func2(30),func2(50),
                 func2(100),func2(300),func2(500))
 
